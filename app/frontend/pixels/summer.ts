0419481e6484d3fb33f4654ecadfee6a47a6d7ad
Dmitry Golovin <headman.dev@gmail.com>
2024-01-29 22:11:30 -0300

    MutationObserver to track url changes

--- app/frontend/pixels/summer.ts
+++ app/frontend/pixels/summer.ts
@@ -1,14 +1,42 @@
 const observeUrlChange = () => {
-  let oldHref = document.location.href;
+  const oldHref = document.location.href;
   console.log(oldHref);
-  const body = document.querySelector("body");
-  if (!body) return;
-  const observer = new MutationObserver(() => {
-    if (oldHref !== document.location.href) {
-      oldHref = document.location.href;
-      console.log('url changed');
-    }
-  });
-  observer.observe(body, { childList: true, subtree: true });
+  window.addEventListener(
+    "hashchange",
+    () => {
+      console.log("The hash has changed!");
+    },
+    false,
+  );
+  // const body = document.querySelector("body");
+  // if (!body) return;
+  // const observer = new MutationObserver(() => {
+  //   console.log(document.location.href);
+  //   if (oldHref !== document.location.href) {
+  //     oldHref = document.location.href;
+  //     console.log('url changed');
+  //   }
+  // });
+  // observer.observe(body, { childList: true, subtree: true });
 };
 observeUrlChange();
+
+let previousUrl = location.href;
+const observer = new MutationObserver(function() {
+  console.log('mutation', location.href);
+
+  // If URL changes...
+  if (location.href !== previousUrl) {
+    previousUrl = location.href;
+
+    // Close popup
+    // removeBanner();
+
+    // Once navigation is detected, disconnect mutation observer
+    // observer.disconnect();
+  }
+});
+
+// Mutation observer setup
+const config = {subtree: true, childList: true};
+observer.observe(document, config);
