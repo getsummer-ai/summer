a34af8e9ecd9dc497f7d27d944b4ee1fa89c786e
Dmitry Golovin <headman.dev@gmail.com>
2024-02-23 20:41:37 -0300

    Update specs for the button controller

--- spec/controllers/api/v1/button_controller_spec.rb
+++ spec/controllers/api/v1/button_controller_spec.rb
@@ -1,54 +1,72 @@
 # frozen_string_literal: true
 RSpec.describe Api::V1::ButtonController do
   # ActiveRecord::Base.logger = Logger.new(STDOUT) if defined?(ActiveRecord::Base)
-  let!(:project) do
-    user =
-      User.create(
-        email: 'admin@test.ru',
-        password: '12345678',
-        password_confirmation: '12345678',
-        confirmed_at: Time.zone.now,
-      )
-    user.projects.create!(domain: 'localhost.com', name: 'Test Project')
+  let!(:user) do
+    User.create(
+      email: 'admin@test.ru',
+      password: '12345678',
+      password_confirmation: '12345678',
+      confirmed_at: Time.zone.now,
+    )
   end
-  let(:api_key) { project.uuid }
+  let!(:project) { user.projects.create!(domain: 'localhost.com', name: 'Test Project') }
+  let!(:api_key) { project.uuid }
 
-  before do
-    request.headers['Api-Key'] = api_key
-  end
+  before { request.headers['Api-Key'] = api_key }
 
   describe 'POST /init' do
+    subject(:post_request) do
+      post(:init, body: { s: 'http://localhost:3000/new-year-celebrations' }.to_json, as: :json)
+    end
+
     context 'when api key is wrong' do
       let(:api_key) { project.id }
 
       it 'returns Invalid Api-Key response' do
-        post(:init, body: { s: 'http://localhost:3000/new-year-celebrations' })
+        post_request
         expect(response).to have_http_status(:forbidden)
         expect(response.body).to eq "{\"message\":\"Invalid Api-Key\"}"
       end
     end
 
     it 'returns Incorrect domain response' do
-      post(:init, body: { s: 'http://localhost:3000/new-year-celebrations' })
+      post_request
       expect(response).to have_http_status(:forbidden)
       expect(response.body).to eq "{\"code\":\"wrong_domain\",\"message\":\"Incorrect domain\"}"
     end
-    
+
     describe 'when http_status is 200' do
       render_views
-      it 'returns response successfully' do
+      before do
         project.update_attribute!(:domain, 'localhost')
         request.headers['origin'] = 'http://localhost'
-
         stub_request(:get, 'http://localhost:3000/new-year-celebrations').to_return(
-        body: Rails.root.join('spec/fixtures/html/new-year-celebrations.html').read,
-        headers: { 'Content-Type' => 'text/html' }
+          body: Rails.root.join('spec/fixtures/html/new-year-celebrations.html').read,
+          headers: {
+            'Content-Type' => 'text/html',
+          },
         )
+      end
 
-        post(:init, body: { s: 'http://localhost:3000/new-year-celebrations' }.to_json, as: :json)
+      it 'saves parsed data in database' do
+        post_request
+        expect(response).to have_http_status(:ok)
+        expect([ProjectArticle.count, ProjectUrl.count, ProjectArticle.first.project_urls.count]).to eq [1,1,1]
+        article = project.project_articles.first
+        expect(article.tokens_count).to be_a(Integer)
+        expect(article.title).to eq 'New Year celebrations'
+        expect(article.status_summary).to eq 'wait'
+        expect(article.status_services).to eq 'wait'
+
+        url = project.project_urls.first
+        expect(url.url).to eq 'http://localhost:3000/new-year-celebrations'
+        expect(url.is_accessible).to be true
+      end
+
+      it 'returns response successfully' do
+        post_request
         expect(response).to have_http_status(:ok)
         body = response.parsed_body
-        # expect(body['article']).to have_key(:id)
         expect(body['article']['id']).to be_a(String)
         expect(body['article']['title']).to eq 'New Year celebrations'
         expect(body['settings']).to be_a(Hash)
