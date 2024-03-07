acf070767aa54ecff63047187e7459420f57b12e
Dmitry Golovin <headman.dev@gmail.com>
2024-03-06 21:19:46 -0300

    Continue changing the views

--- app/builders/daisy_builder.rb
+++ app/builders/daisy_builder.rb
@@ -3,23 +3,23 @@ class DaisyBuilder < ActionView::Helpers::FormBuilder
   # include ActionView::Context
 
   def label(attribute, text = nil, options = {}, &)
-    return super(attribute, text, options.reverse_merge(class: 'label pb-1'), &) if block_given?
+    return super(attribute, text, options.reverse_merge(class: 'label label-summer'), &) if block_given?
 
-    super(attribute, options.reverse_merge(class: 'label pb-1')) do |builder|
+    super(attribute, options.reverse_merge(class: 'label label-summer')) do |builder|
       content_tag(:span, text.presence || builder.translation, class: 'label-text')
     end
   end
 
   def email_field(attribute, options = {})
-    super(attribute, options.reverse_merge(class: 'input input-bordered input-sm input-ghost rounded-none'))
+    super(attribute, options.reverse_merge(class: 'input input-summer'))
   end
 
   def text_field(attribute, options = {})
-    super(attribute, options.reverse_merge(class: 'input input-bordered input-sm input-ghost rounded-none'))
+    super(attribute, options.reverse_merge(class: 'input input-summer'))
   end
 
   def password_field(attribute, options = {})
-    super(attribute, options.reverse_merge(class: 'input input-bordered input-sm input-ghost rounded-none'))
+    super(attribute, options.reverse_merge(class: 'input input-summer'))
   end
 
   def check_box(attribute, options = {})
