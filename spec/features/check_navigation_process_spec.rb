b3f8e396b73d72d402cebe41fe6246c3b1a292db
Dmitry Golovin <headman.dev@gmail.com>
2024-05-03 18:57:25 -0300

    Add wait in case the modal has not been loaded yet

--- spec/features/check_navigation_process_spec.rb
+++ spec/features/check_navigation_process_spec.rb
@@ -104,7 +104,7 @@ describe 'the Navigation process' do
 
         refresh
 
-        # expect(page).to have_content 'Summer will appear on all the pages from your domain link'
+        expect(page).to have_field(with: article.title, wait: 3)
 
         within_table("table-page-statistics") do
           expect(page).to have_content 'Summary'
