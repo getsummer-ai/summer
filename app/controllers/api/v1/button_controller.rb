733e6f9a087eb9f4d006e18786b70d0b5bc89937
Dmitry Golovin <headman.dev@gmail.com>
2024-05-11 17:18:15 -0300

    Version 1.8.0

--- app/controllers/api/v1/button_controller.rb
+++ app/controllers/api/v1/button_controller.rb
@@ -7,7 +7,7 @@ module Api
     class ButtonController < Api::V1::ApplicationController
       before_action :validate_init_request, only: :init
 
-      BUTTON_VERSION = '0.1.7'
+      BUTTON_VERSION = '0.1.8-beta'
 
       def settings
         @app_path =
