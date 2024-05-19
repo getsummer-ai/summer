6cba90b0750cdc0c67e22aca382ff5d351021314
Dmitry Golovin <headman.dev@gmail.com>
2024-05-19 13:35:41 -0300

    Fix UI a little

--- app/components/actions/action_card_component.rb
+++ app/components/actions/action_card_component.rb
@@ -3,10 +3,11 @@ module Actions
   class ActionCardComponent < ViewComponent::Base
     include ViewComponent::UseHelpers
 
-    def initialize(title:, description: '', color: 'pink')
+    def initialize(title:, description: '', color: 'pink', link: nil)
       @title = title
       @description = description
       @color = color
+      @link = link
 
       super
     end
