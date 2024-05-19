bc56b55337271dfd5b1a58d4a95c8326c7c625f0
Dmitry Golovin <headman.dev@gmail.com>
2024-05-19 16:20:03 -0300

    Fix avo admin panel

--- app/controllers/avo/project_pages_controller.rb
+++ app/controllers/avo/project_pages_controller.rb
@@ -2,7 +2,7 @@
 # This controller has been generated to enable Rails' resource routes.
 # More information on https://docs.avohq.io/2.0/controllers.html
 module Avo
-  class ProjectUrlsController < Avo::ResourcesController
+  class ProjectPagesController < Avo::ResourcesController
 
     def update
       @record.start_tracking(author: _current_user, source: 'Admin Actions')
