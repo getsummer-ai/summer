7a3991d9f1afdb9b911b0e98aab617027c75f198
Dmitry Golovin <headman.dev@gmail.com>
2024-07-25 19:29:40 -0300

    Add app error state

---
+++ app/frontend/svelte/apps/summer/components/ErrorBlock.svelte
@@ -0,0 +1,14 @@
+<script lang="ts"></script>
+
+<div class="error-block">
+  Whooops, something went wrong. <br>
+  The summary can’t be displayed now. <br>
+  Try again or check the full article.
+</div>
+
+<style lang="scss">
+  .error-block {
+    margin-top: 40px;
+    text-align: center;
+  }
+</style>
