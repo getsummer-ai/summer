55c7e27074ac04cf885eb710415c5c64afd14267
Dmitry Golovin <headman.dev@gmail.com>
2024-08-04 17:29:20 -0300

    Change feature icons

--- app/components/actions/action_card_component.rb
+++ app/components/actions/action_card_component.rb
@@ -3,11 +3,12 @@ module Actions
   class ActionCardComponent < ViewComponent::Base
     include ViewComponent::UseHelpers
 
-    def initialize(title:, description: '', color: 'pink', link: nil)
+    def initialize(title:, icon:, description: '', color: 'pink', link: nil)
       @title = title
       @description = description
       @color = color
       @link = link
+      @icon = icon
 
       super
     end
