5a75852b021666fd4f85cba4b7e3d744e3b5b91d
Dmitry Golovin <headman.dev@gmail.com>
2024-08-16 16:06:33 -0300

    Change animation of the button and icons within

--- app/frontend/svelte/apps/summer/Summer.svelte
+++ app/frontend/svelte/apps/summer/Summer.svelte
@@ -6,6 +6,8 @@
   import ProjectProduct from './ProjectProduct.svelte';
   import SubscriptionBlock from './SubscriptionBlock.svelte';
   import ErrorBlock from './components/ErrorBlock.svelte';
+  import CloseIcon from './components/CloseIcon.svelte';
+  import LoadingIcon from './components/LoadingIcon.svelte';
   import markdown from './markdown.js';
 
   /* eslint svelte/no-at-html-tags: 0 */
@@ -15,15 +17,16 @@
   export let settings: SettingsInfo;
   export let article: ArticleInitInfo;
   let summary: string = '';
+  let button: HTMLButtonElement;
   let services: ProjectProductType[] = [];
   let isSummaryCompleted: boolean = false;
   let loading = false;
   let showButton = false;
-  let buttonStyles = getButtonStyles();
   const rootDiv = document.getElementById(rootDivId);
   const api = initApi(projectId);
 
   $: isError = summary.includes('--ERROR--');
+  $: strokeColor = settings.appearance.button_theme === 'white' ? 'black' : 'white';
 
   const mousewheelEvent = (e: WheelEvent) => {
     const target = e.target as HTMLElement;
@@ -44,19 +47,11 @@
     rootDiv.removeEventListener('mousewheel', mousewheelEvent);
   });
 
-  function getButtonStyles(width = 109): { width: string; left: number } {
-    return {
-      width: width === 0 ? 'auto' : `${width}px`,
-      left: (width === 0 ? 146.3 : width) / 2,
-    };
-  }
-
   const openModal = async (delay = 100) =>
     new Promise((resolve) => {
       setTimeout(() => {
         showModal = true;
         resolve();
-        buttonStyles = getButtonStyles(87.5);
       }, delay);
     });
 
@@ -64,7 +59,6 @@
     if (loading) return;
     if (showModal) return closeModal();
     if (summary) return openModal();
-    buttonStyles = getButtonStyles(0);
 
     loading = true;
 
@@ -82,8 +76,7 @@
           isSummaryCompleted = true;
           retrieveProducts();
         });
-        buttonStyles = getButtonStyles(146.3);
-        await openModal(500);
+        await openModal(700);
       } catch (error) {
         // console.log(error);
       } finally {
@@ -104,7 +97,6 @@
   };
 
   const closeModal = () => {
-    buttonStyles = getButtonStyles();
     loading = false;
     showModal = false;
   };
@@ -118,34 +110,15 @@
 </svelte:head>
 
 <button
+  bind:this={button}
   class="getsummer-btn theme-{settings.appearance.button_theme} {showModal ? 'active' : ''}"
   on:click={onButtonClick}
-  style="--summer-button-width: {buttonStyles.left + 'px'}; width: {buttonStyles.width}"
 >
   {#if showModal}
-    <svg
-      class="icon"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M8 8L12 12M12 12L8 16M12 12L16 16M12 12L16 8"
-        stroke={settings.appearance.button_theme === 'white' ? 'black' : 'white'}
-      />
-      <circle
-        cx="12"
-        cy="12"
-        r="10.5"
-        stroke={settings.appearance.button_theme === 'white' ? 'black' : 'white'}
-        stroke-opacity="0.2"
-      />
-    </svg>
+    <CloseIcon class="btn-icon" {strokeColor} />
     <span>Close</span>
   {:else if loading === true}
-    <span class="loading-icon" />
+    <LoadingIcon class="btn-icon" {strokeColor} />
     <span>Summarizing</span>
   {:else}
     Summarize
@@ -164,7 +137,7 @@
     {:else}
       {#if summary.length === 0}
         <div class="loading-block">
-          <span class="loading-icon" />
+          <LoadingIcon width={40} {strokeColor} />
         </div>
       {/if}
 
@@ -197,6 +170,10 @@
     border-style: solid;
   }
 
+  :global(.btn-icon) {
+    @apply ml-[-9px] mr-1;
+  }
+
   @keyframes smooth-appear {
     0%,
     50% {
@@ -222,13 +199,13 @@
     text-align: left;
     height: 32px;
     word-break: keep-all;
-    left: calc(50% - var(--summer-button-width));
+    left: 50%;
+    transform: translateX(-50%);
     bottom: 20px;
     box-shadow: 0 36px 60px 0 rgba(0, 0, 0, 0.18), 0 13.902px 18.888px 0 rgba(0, 0, 0, 0.12),
       0 6.929px 11.218px 0 rgba(0, 0, 0, 0.1), 0 3.621px 7.444px 0 rgba(0, 0, 0, 0.08),
       0 1.769px 4.735px 0 rgba(0, 0, 0, 0.06), 0 0.664px 2.345px 0 rgba(0, 0, 0, 0.03);
     backdrop-filter: blur(4px);
-    transition: width 0.3s;
     animation: smooth-appear 400ms ease-in-out;
     z-index: 10001;
 
@@ -236,10 +213,6 @@
       @media (max-width: 640px) {
         display: none;
       }
-    }
-
-    .icon {
-      @apply ml-[-9px] mr-1;
     }
 
     &.theme-white {
@@ -259,21 +232,13 @@
     }
   }
 
-  .loading-icon {
-    @apply loading loading-spinner loading-sm align-text-bottom mr-1 opacity-80;
-  }
-
   .loading-block {
     @apply text-center;
-    padding-top: 80px;
-    opacity: 0.2;
+    padding-top: 100px;
+    opacity: 0.4;
 
     @media (max-width: 640px) {
       padding-top: 120px;
-    }
-
-    .loading-icon {
-      @apply loading-lg;
     }
   }
 </style>
---
+++ app/frontend/svelte/apps/summer/components/LoadingIcon.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  export let strokeColor: string;
+  export let width: number = 24;
+  console.log($$restProps.class)
+</script>
+
+<svg
+  class={ 'animate ' + ($$restProps.class ?? '') }
+  xmlns="http://www.w3.org/2000/svg"
+  width={width}
+  height={width}
+  viewBox="0 0 24 24"
+  fill="none"
+>
+  <circle cx="12" cy="12" r="8.5" stroke={strokeColor} stroke-opacity="0.2" />
+  <path
+    d="M12 3.5C13.3044 3.5 14.5912 3.80019 15.761 4.37732C16.9307 4.95446 17.9519 5.79305 18.7455 6.82818C19.5392 7.86332 20.0839 9.06721 20.3377 10.3467C20.5914 11.6261 20.5472 12.9468 20.2086 14.2064C19.87 15.4661 19.2461 16.6309 18.3851 17.6107C17.5241 18.5906 16.4492 19.3591 15.2435 19.8568C14.0379 20.3545 12.7338 20.5681 11.4324 20.481C10.1309 20.3939 8.86699 20.0085 7.73841 19.3545"
+    stroke={strokeColor}
+  />
+</svg>
+
+<style lang="scss">
+  .animate {
+    @apply animate-spin;
+  }
+</style>
