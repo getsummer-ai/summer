30e8001bd56d024415e0b4c072a93aa48910442c
Dmitry Golovin <headman.dev@gmail.com>
2024-09-25 19:46:10 -0300

    Change the LLM model to the cheapest one if an article is too large (> 10 000 tokens)

--- app/jobs/summarize_article_job.rb
+++ app/jobs/summarize_article_job.rb
@@ -12,20 +12,26 @@ class SummarizeArticleJob < ApplicationJob
     model = ProjectArticle.includes(:project).find(id)
     project = model.project
 
-    model_wrapper = SummarizeArticleService.new(
-      model:,
-      llm: llm_for(project),
-      guidelines: project.guidelines
-    )
+    model_wrapper =
+      SummarizeArticleService.new(
+        model:,
+        llm: llm_for(project, model),
+        guidelines: project.guidelines,
+      )
     model_wrapper.summarize
   end
 
   protected
 
   # @param [Project] project
-  def llm_for(project)
+  # @param [ProjectArticle] model
+  def llm_for(project, model)
     res = Project.default_llms[project.default_llm]
-    res = Project.default_llms[:gpt_4o_mini] if Rails.env.development?
+
+    if Rails.env.development? || model.tokens_count > 10_000
+      res = Project.default_llms[:gpt_4o_mini]
+    end
+
     res.to_s
   end
 end
