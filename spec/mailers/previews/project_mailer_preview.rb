74556d5ffb65f23de5bfded63de205e569a3affb
Dmitry Golovin <headman.dev@gmail.com>
2024-09-25 18:52:00 -0300

    Update free plan suspension email

--- spec/mailers/previews/project_mailer_preview.rb
+++ spec/mailers/previews/project_mailer_preview.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 class ProjectMailerPreview < ActionMailer::Preview
   def suspension_notification_free_plan
-    ProjectMailer.suspension_notification(Project.where(plan: 'free').first.id)
+    ProjectMailer.out_of_clicks_suspension_notification(334)
   end
 
   def suspension_notification_light_plan
-    ProjectMailer.suspension_notification(Project.where(plan: 'light').first.id)
+    ProjectMailer.out_of_clicks_suspension_notification(Project.where(plan: 'light').first.id)
   end
 
   def suspension_notification_pro_plan
-    ProjectMailer.suspension_notification(Project.where(plan: 'pro').first.id)
+    ProjectMailer.out_of_clicks_suspension_notification(Project.where(plan: 'pro').first.id)
   end
 end
